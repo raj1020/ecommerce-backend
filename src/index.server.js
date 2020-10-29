4d14b728dc1f0ac17963e60082bcaea140cfacb5
raj1020 <raj.1020@hotmail.com>
2020-10-29 12:05:24 +1100

    added nodemon and bodyParser node packages and defined a post route

--- src/index.server.js
+++ src/index.server.js
@@ -1,12 +1,22 @@
 const express = require ('express');
 const env = require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
+
 
 env.config();
+
+app.use(bodyParser());
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Hello! This is the server."
+    });
+
+})
+app.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
     });
 
 })
