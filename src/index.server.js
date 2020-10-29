49f39b452f3d844829c67f62d2ccd4d3d1aa2686
raj1020 <raj.1020@hotmail.com>
2020-10-30 10:39:50 +1100

    connected the backend to Mongodb

--- src/index.server.js
+++ src/index.server.js
@@ -2,11 +2,24 @@ const express = require ('express');
 const env = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+
+// mongodb+srv://Prad:<password>@cluster0.rxy5s.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 env.config();
 
-app.use(bodyParser());
+mongoose.connect(
+    'mongodb://localhost:27017/test', 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('MongoDB connected');
+    })
+
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
@@ -25,3 +38,4 @@ app.post('/', (req, res, next) => {
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}` );
 })
+
