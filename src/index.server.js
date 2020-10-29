f38d94df73d9f0d5010c754f28389c27a014d184
raj1020 <raj.1020@hotmail.com>
2020-10-29 11:37:16 +1100

    started backend for ecommerce project

---
+++ src/index.server.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const env = require('dotenv');
+const app = express();
+
+env.config();
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Hello! This is the server."
+    });
+
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}` );
+})
